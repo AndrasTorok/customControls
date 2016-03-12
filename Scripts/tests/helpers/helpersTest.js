954f4d6ef2c0461989bce0ab3e3947cec2b7dc00
Andras <andras.torok@yahoo.com>
2016-03-12 08:30:44 +0200

    Entities for composed PK

--- Scripts/tests/helpers/helpersTest.js
+++ Scripts/tests/helpers/helpersTest.js
@@ -1,28 +1,56 @@
 ﻿describe('Entities', function () {
     var Person = (function () {
 
-        var ctor = function (firstName, lastName) {
+        var ctor = function (firstName, lastName, genre) {
             this.id = Helpers.Guid.newGuid();
             this.firstName = firstName;
             this.lastName = lastName;
+            this.updateGenre(genre);
+        }
+
+        ctor.prototype.updateGenre = function (genre) {
+            this.genre = new PersonGenre(this.id, genre);
         }
 
         return ctor;
     })();
 
-    var persons;
+    var Genre = {
+        Unknown : 0,
+        ClassicalCrossOver: 1,
+        Pop: 2,
+        Trance : 3
+    };
+
+    var PersonGenre = (function () {
+
+        var ctor = function (personId, genre) {
+            this.personId = personId;
+            this.genre = genre;
+        }
+
+        return ctor;
+    })();
+
+    var persons,
+        personsGenres,
+        davidBowie,
+        peterGabiel,
+        arminVanBuuren,
+        sarahBrightman;
 
     beforeEach(function () {
-        persons = [
-            new Person('David', 'Bowie'),
-            new Person('Peter', 'Gabriel'),
-            new Person('Armin', 'van Buuren'),
-            new Person('Andrea', 'Bocelli'),
-            new Person('Sarah', 'Brightman'),
-        ];
+        davidBowie = new Person('David', 'Bowie', Genre.Pop);
+        peterGabiel = new Person('Peter', 'Gabriel', Genre.Unknown);
+        arminVanBuuren = new Person('Armin', 'van Buuren', Genre.Trance);
+        andreaBocelli = new Person('Andrea', 'Bocelli', Genre.ClassicalCrossOver);
+        sarahBrightman = new Person('Sarah', 'Brightman', Genre.ClassicalCrossOver);
+        persons = [davidBowie, peterGabiel, arminVanBuuren, andreaBocelli, sarahBrightman];
+        personsGenres = [davidBowie.genre, peterGabiel.genre, arminVanBuuren.genre, andreaBocelli.genre, sarahBrightman.genre];
+
     });
 
-    it('changed works ok', function () {
+    it('changed OK for simple PK', function () {
         var entities = new Helpers.Entities({
             pKeys: ['id'],
             entities: persons
@@ -42,5 +70,26 @@
 
         expect(changes.changed.length).toEqual(1);
         expect(changes.changed[0].lastName).toEqual('Gabriel');
+    });
+
+    it('changed OK for composed PK', function () {
+        var entities = new Helpers.Entities({
+            pKeys: ['personId', 'genre'],
+            entities: personsGenres
+        });
+
+        personsGenres[1].genre = Genre.Pop;                                     //when changed and item in table entities with composed PK
+
+        var changes = entities.changed();                                       //changed result will be
+
+        expect(changes.changed.length).toEqual(0);                              //no entities changed, but...
+
+        expect(changes.deleted.length).toEqual(1);                              //the entity changed appears to be deleted
+        expect(changes.deleted[0].personId).toEqual(peterGabiel.id);
+        expect(changes.deleted[0].genre).toEqual(Genre.Unknown);
+
+        expect(changes.added.length).toEqual(1);                                //and readded
+        expect(changes.added[0].personId).toEqual(peterGabiel.id);
+        expect(changes.added[0].genre).toEqual(Genre.Pop);
     });
 });
